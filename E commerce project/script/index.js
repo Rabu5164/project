920df878ba0993175c39d6ca19c5638556729a70
Rabu5164 <124350635+Rabu5164@users.noreply.github.com>
2025-01-23 19:52:02 +0600

    Add files via upload

---
+++ E commerce project/script/index.js
@@ -0,0 +1,14 @@
+const menuIcon= document.getElementById
+("menu-icon");
+
+const menuIcon= document.getElementById
+("menu");
+menuIcon ?.addEventListener("click",()=>
+{
+  if(menu.className   "hidden") {
+    menu.classlist.remove("hidden");
+  } 
+  else{
+    menu.classlist.add("hidden");
+  }
+});
